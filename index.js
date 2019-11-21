4b1212d2e1ec6900f58136efac1fdd2553c05e0c
ajinoxx <57328840+ajinoxx@users.noreply.github.com>
2019-11-20 19:36:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const client = new Discord.Client();
 var randomNum;
 var youtubeLink;
 var link;
+var tonnID = '645702626104705031';
+var fredboatID = '184405311681986560';
+var mee6ID = '159985870458322944';
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -18,9 +21,9 @@ client.on('message', message => {
         message.channel.send("Live demo?")
     else if(userMsg.startsWith('what') && userMsg.length == 4)
         message.channel.send("Am I talking to myself?")
-    else if (message.author.id != '645702626104705031'){
+    else if (message.author.id != tonnID && message.author.id != fredboatID && message.author.id != mee6ID){
         randomNum = Math.random() * 100;
-        if (randomNum < 5){
+        if (randomNum < 3){
             randomNum = Math.random() * 100;
             if(randomNum >= 33)
                 message.channel.send("That's a note!");
